1a73fec974202256e1dde899bb9f344815443bcb
Yaroslav Magrelo <slava.magrelo@gmail.com>
2021-05-24 21:52:50 +0300

    add HookSwitcher component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HookSwitcher />
     </div>
   );
+}
+
+const HookSwitcher = () => {
+  const [ themeColor, setThemeColor ] = useState('white');
+  
+  return (
+    <div style={{ padding: '10px', backgroundColor: themeColor}}>
+      <button onClick={() => {setThemeColor('black')}}>Dark</button>
+      <button onClick={() => {setThemeColor('white')}}>Light</button>
+    </div>
+  )
 }
 
 export default App;
